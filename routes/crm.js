3514e433135ea3224f6d0d881e6269da8f37eaf5
Charlotte Oury <charlotte.ouryb@gmail.com>
2020-12-14 16:35:22 +0100

    start client page and client route

--- routes/crm.js
+++ routes/crm.js
@@ -12,8 +12,16 @@ router.get("/account-management", (req, res) => {
   res.send("hey");
 });
 
-router.get("/account-management/:id", (req, res) => {
-  res.send("hey");
+router.get("/account-management/:id", async (req, res, next) => {
+  try {
+    res.render("client_page", await ClientModel.findById(req.params.id));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/user/:id", (req, res) => {
+  res.render("user_page");
 });
 
 module.exports = router;
