b60292f54339c8168b6ba4f6b5e67ac18e77dd1d
Calypso Asline <aslinecalypso@gmail.com>
2020-12-15 15:57:39 +0100

    burger

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
 // to slide/close the burger menu
 const slide = document.querySelector(".slide");
 const close = document.querySelector(".close");
+const burger = document.getElementById("burger");
+console.log(burger);
 
 function openSlideMenu() {
-  document.getElementById("menu").style.width = "150px";
-  document.getElementById("content").style.marginLeft = "150px";
+  document.getElementById("menu").style.width = "100px";
+  document.getElementById("content").style.marginLeft = "100px";
 }
 
 function closeSlideMenu() {
@@ -14,3 +16,11 @@ function closeSlideMenu() {
 
 slide.addEventListener("click", openSlideMenu);
 close.addEventListener("click", closeSlideMenu);
+
+burger.addEventListener("click", () => {
+  if (burger.style.display === "none") {
+    burger.style.display = "block";
+  } else {
+    burger.style.display = "none";
+  }
+});
